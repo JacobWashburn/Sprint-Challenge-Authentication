a157807ee447ace64e3f7ed2777e51f18d759d87
Jacob Washburn <jacobrwashburn@gmail.com>
2020-01-31 11:14:01 -0700

    created login endpoint and created token

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -22,7 +22,34 @@ router.post('/register', (req, res) => {
 });
 
 router.post('/login', (req, res) => {
-  // implement login
+    let {username, password} = req.body;
+    db.findBy({username})
+        .then(user => {
+            if (user && bcrypt.compareSync(password, user.password)) {
+                const token = signToken(user);
+                res.status(200).json({
+                    message: `Welcome ${user.username}!`,
+                    token
+                });
+            } else {
+                console.log('else user', user);
+                res.status(500).json({message: 'Invalid Credentials'});
+            }
+        })
+        .catch(error => {
+            console.log('login error', error);
+            res.status(500).json({message: 'login error'});
+        });
 });
+
+function signToken(user) {
+    const payload = {
+        user
+    };
+    const options = {
+        expiresIn: '1d'
+    };
+    return jwt.sign(payload, jwtSecret, options);
+}
 
 module.exports = router;

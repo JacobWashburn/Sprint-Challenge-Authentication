b52c3591aecd160117641f2e9a145367477b7174
Jacob Washburn <jacobrwashburn@gmail.com>
2020-01-31 11:13:10 -0700

    created register endpoint

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -6,7 +6,19 @@ const {jwtSecret} = require('../config/secrets');
 const db = require('../users/users-model');
 
 router.post('/register', (req, res) => {
-  // implement registration
+    let user = req.body;
+    user.password = bcrypt.hashSync(user.password, 10);
+    db.add(user)
+        .then(addedUser => {
+            res.status(201).json(addedUser);
+        })
+        .catch(error => {
+            console.log('add a user error', error);
+            res.status(500).json({
+                message: 'there was an error adding a user.',
+                error: error
+            });
+        });
 });
 
 router.post('/login', (req, res) => {
